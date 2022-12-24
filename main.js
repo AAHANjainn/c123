c1459426c8468dbe943c60e449025e3d7959b81e
AAHANjainn <92387845+AAHANjainn@users.noreply.github.com>
2022-12-24 12:34:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nose_x = 0;
+nose_y = 0;
+left_wrist_x = 0;
+right_wrist_x = 0;
+gap = 0 ; 
+
 function setup(){
     canvas = createCanvas(500,400);
     video = createCapture(VIDEO);
@@ -14,5 +20,18 @@ function modelLoaded(){
 function gotResults(results){
     if(results.length > 0){
         console.log(results);
+        nose_x = results[0].pose.nose.x;
+nose_y = results[0].pose.nose.y;
+left_wrist_x = results[0].pose.leftWrist.x;
+right_wrist_x = results[0].pose.rightWrist.x;
+gap = left_wrist_x - right_wrist_x ; 
     }
+}
+
+function draw(){
+    background("white")
+    text("Aahan",nose_x,nose_y);
+    textSize(gap);
+    document.getElementById("fontsize").innerHTML = Math.round(gap)+"px";
+
 }
